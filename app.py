f1f933b338a368e00bd43c57793a6f5fc526590b
SebasFlo_20 <sebas08fr@gmail.com>
2024-11-03 23:23:34 -0500

    errores arreglados

--- app.py
+++ app.py
@@ -1,57 +1,60 @@
 import sqlite3
-from flask import Flask, render_template, abort, request, url_for
-# from db import get_db_connection
+from flask import Flask, render_template, abort, request, url_for, redirect
+from db import get_db_connection
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-        return render_template('index.html')
+        return render_template('base.html')
 
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html')
 
-@app.route('/post', methods=['GET'])
+@app.route("/post", methods=["GET"])
 def get_all_post():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchone()
+    posts = conn.execute("SELECT * FROM posts").fetchall()
     conn.close()
-    return render_template('post/posts.html', posts=posts)
+    return render_template(template_name_or_list="post/posts.html", posts=posts)
 
 @app.route('/post/<int:post_id>', methods=['GET'])
 def get_one_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
-    conn.close()
-    return render_template('post/post.html', post=post)
-
-@app.route('/post/create', methods=['POST'])
+    if request.method == "GET":
+        conn = get_db_connection()
+        post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+        conn.close()
+        if post is None:
+            abort(404)
+        return render_template(template_name_or_list="post/post.html", post=post)
+    
+@app.route('/post/create', methods=['GET','POST'])
 def create_one_post():
     if request.method == 'POST':
-        title = request.form['title']
-        contend = request.form['contend']
+        tittle = request.form['tittle']
+        content = request.form['content']
         conn = get_db_connection()
-        conn.execute('INSERT INTO posts (title, content) VALUES (?, !)', (title, contend))
+        conn.execute('INSERT INTO posts (tittle, content) VALUES (?, ?)', (tittle, content))
         conn.commit()
         conn.close()
-        return redirect(url_for('/post'))
+        return redirect(url_for("get_all_post"))
 
     return render_template('post/create.html')
 
 
-@app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
-def edit_one_post(id):
+@app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
     
     if request.method == 'POST':
-        title = request.form['title']
+        tittle = request.form['tittle']
         content = request.form['content']
 
         conn = get_db_connection()
-        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+        conn.execute('UPDATE posts SET tittle = ?, content = ? WHERE id = ?', (tittle, content, post_id))
         conn.commit()
         conn.close()
         return redirect(url_for('get_all_post'))
@@ -60,15 +63,15 @@ def edit_one_post(id):
         return render_template('post/edit.html', post=post)
 
 
-@app.route('/post/delete/<str:post_id>', methods=['POST'])
+@app.route('/post/delete/<int:post_id>', methods=['POST'])
 def delete_one_post(post_id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (post_id))
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
     conn.commit()
     conn.close()
 
-    return redirect(url_for('get_all_post')
+    return redirect(url_for('get_all_post'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host=)
+    app.run(debug=True)
 ########################################################################## END BLOQUE 2
